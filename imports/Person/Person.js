d24d4659c7f0991d9de7a89697af913a0b694d78
Antonin Adert <antonin.adert@gmail.com>
2017-08-30 11:30:56 +0200

    Added semantic-ui dependencies

--- imports/Person/Person.js
+++ imports/Person/Person.js
@@ -5,6 +5,9 @@ Person({
       Name: <input b="value: name" /><br />
       <label b="text: 'Hello ' + name"/>
       <a href="/about">About Us</a>
+    <button class="ui primary button">
+    Save
+  </button>
     </div>
   }
 });
